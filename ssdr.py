e67ca684f9959ccc20552df3b746fe27fd8a36e3
Bobbie Chen <bobbiechen75@gmail.com>
2020-04-28 23:48:15 -0400

    Replace self-links to direct to the proxy

--- ssdr.py
+++ ssdr.py
@@ -1,6 +1,7 @@
 """
-This example shows how to send a reply from the proxy immediately
-without sending any data to the remote server.
+This script auto-inlines images and Javascript via mitmproxy.
+
+TODO: CSS
 """
 import base64
 import functools
@@ -8,36 +9,41 @@ import requests
 
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
-from mitmproxy import http
+from mitmproxy import http, ctx
 
 
 MAX_WORKERS = 6
 
 
-def img_retrieve_source_base64(flow, old_src):
+def img_retrieve_source_base64(base_uri: str, old_src: str):
     try:
         resp = requests.get(old_src)
     except requests.exceptions.MissingSchema:
-        new_uri = f"{flow.request.scheme}://{flow.request.host}:{flow.request.port}/{old_src}"
+        new_uri = f"{base_uri}/{old_src}"
         resp = requests.get(new_uri)
-    except Exception:
+    except Exception as e:
+        ctx.log.error(f"Error in img {old_src}: {e}")
         return old_src
     return f"data:{resp.headers['content-type']};base64,{base64.b64encode(resp.content).decode()}"
 
 
-def script_get_source_string(flow, old_src):
+def script_get_source_string(base_uri: str, old_src: str):
     try:
         resp = requests.get(old_src)
     except requests.exceptions.MissingSchema:
-        new_uri = f"{flow.request.scheme}://{flow.request.host}:{flow.request.port}/{old_src}"
+        new_uri = f"{base_uri}/{old_src}"
         resp = requests.get(new_uri)
-    except Exception:
+    except Exception as e:
+        ctx.log.error(f"Error in img {old_src}: {e}")
         return ''
     return resp.content.decode()
 
 
 def response(flow: http.HTTPFlow) -> None:
     # flow: a mitmproxy.http.HTTPFlow
+    original_base_uri = f"{flow.request.scheme}://{flow.request.host}:{flow.request.port}"
+    new_base_uri = f"{flow.request.scheme}://{ctx.options.listen_host}:{ctx.options.listen_port}"
+
     html = BeautifulSoup(flow.response.content, "html.parser")
     if html.body:
         scripts = [script for script in html.findAll('script') if script.get('src')]
@@ -47,11 +53,11 @@ def response(flow: http.HTTPFlow) -> None:
         num_workers = MAX_WORKERS or (len(scripts) + len(images) + len(styles))
         with ThreadPoolExecutor(max_workers=num_workers) as executor:
             image_results = [
-                executor.submit(functools.partial(img_retrieve_source_base64, flow), image['src'])
+                executor.submit(functools.partial(img_retrieve_source_base64, original_base_uri), image['src'])
                 for image in images
             ]
             script_results = [
-                executor.submit(functools.partial(script_get_source_string, flow), script['src'])
+                executor.submit(functools.partial(script_get_source_string, original_base_uri), script['src'])
                 for script in scripts
             ]
 
@@ -63,3 +69,5 @@ def response(flow: http.HTTPFlow) -> None:
                 script.string = result.result()
 
         flow.response.content = str(html).encode("utf8")
+
+    flow.replace(original_base_uri, new_base_uri)
